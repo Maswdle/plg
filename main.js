10ee8dd2faa9119b917218e60b6f22f4fd931d71
Maswdle <103916104+Maswdle@users.noreply.github.com>
2023-06-17 15:34:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -133,17 +133,18 @@ function refresh() {
         var child = mydiv.children[i];
         // console.log(child.querySelector("#nickname").innerHTML);
 
-
+        var t = document.querySelector("#user_list").children[i].getElementsByClassName("nickname").item(0).innerText
         for (const [key, value] of Object.entries(dic_userlist)) {
             //document.querySelector("#user_list").children[i].getElementsByClassName("nickname").item(0).innerText
             if (value.indexOf(child.getElementsByClassName("nickname").item(0).innerText) !== -1 && value.indexOf("女") !== -1) {
                 document.querySelector("#user_list").children[i].getElementsByClassName("nickname").item(0).setAttribute('style', 'background-color: #8b0000;')
-                document.querySelector("#user_list").children[i].getElementsByClassName("nickname").item(0).innerText += "(女)"
+                document.querySelector("#user_list").children[i].getElementsByClassName("nickname").item(0).innerText = "     " + t + "(女)"
+
                 break;
             }
             else if (value.indexOf(child.getElementsByClassName("nickname").item(0).innerText) !== -1) {
                 document.querySelector("#user_list").children[i].getElementsByClassName("nickname").item(0).setAttribute('style', 'background-color: ##009000;')
-                document.querySelector("#user_list").children[i].getElementsByClassName("nickname").item(0).innerText += "(男)"
+                document.querySelector("#user_list").children[i].getElementsByClassName("nickname").item(0).innerText = "     " + t + "(男)"
                 break;
             }
         }
